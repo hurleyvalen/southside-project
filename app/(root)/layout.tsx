1d0cf10ad32bc8e48917494fe2a53fc2d7f4b5be
valen <75712068+hvalensalig@users.noreply.github.com>
2025-01-20 15:27:57 +0800

    added fonts, navbar, logo. modified globals,layout

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,15 @@
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+
+export default function Layout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <main className="font-poppins">
+      <Navbar />
+
+      {children}
+      <Footer />
+    </main>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,51 @@
-import type { Config } from "tailwindcss";
+import type {Config} from "tailwindcss";
 
-export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+const config: Config = {
+    darkMode: ["class"],
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+        "./sanity/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            screens: {
+                xs: "475px",
+            },
+            colors: {
+              earthyBrown: '#6F4E37',
+              creamyBeige: '#FFF5E1',
+              forestGreen: '#228B22',
+              mutedOrange: '#FF8C42',
+              softBlue: '#A9D6E5',
+              charcoalGray: '#2F4F4F',
+              goldenYellow: '#FFD700',
+              sageGreen: '#C5D7B6',
+              skyBlue: '#A8D0DB',
+              cream: '#F5F0E1',
+              dustyRose: '#E3B4B3',
+              mochaBrown: '#D0B8A8',
+              lavenderMist: '#D8C8E0',
+              blushPeach: '#F6C8B8',
+            },
+            fontFamily: {
+                "work-sans": ["var(--font-work-sans)"],
+                "poppins": ["var(--font-poppins)"],
+            },
+            borderRadius: {
+                lg: "var(--radius)",
+                md: "calc(var(--radius) - 2px)",
+                sm: "calc(var(--radius) - 4px)",
+            },
+            boxShadow: {
+                100: "2px 2px 0px 0px rgb(0, 0, 0)",
+                200: "2px 2px 0px 2px rgb(0, 0, 0)",
+                300: "2px 2px 0px 2px rgb(238, 43, 105)",
+            },
+        },
     },
-  },
-  plugins: [],
-} satisfies Config;
+    plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
+};
+
+export default config;
